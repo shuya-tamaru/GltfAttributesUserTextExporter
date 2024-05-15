16024c9f48d82ee0cd459af18335271049b433fa
shuya-yamaru <tamaru1013@gmail.com>
2024-05-15 15:27:05 +0900

    initial commit

---
+++ src/GltfAttributesExporterCommand.cs
@@ -0,0 +1,28 @@
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+using System;
+using System.Collections.Generic;
+
+namespace GltfAttributesExporter
+{
+    public class GltfAttributesExporterCommand : Command
+    {
+        public GltfAttributesExporterCommand()
+        {
+            Instance = this;
+        }
+
+        public static GltfAttributesExporterCommand Instance { get; private set; }
+
+        public override string EnglishName => "GltfAttributesExport";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            RhinoApp.WriteLine("The {0} command is under construction.", EnglishName);
+            return Result.Success;
+        }
+    }
+}
